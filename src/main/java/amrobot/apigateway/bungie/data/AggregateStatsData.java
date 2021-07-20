8b7420dee26482de541882f796f46da8ef6f4231
Vladislavs Zaharovs <vladislav.zaharov@gmail.com>
2021-07-20 04:39:33 +0300

    Rename package

--- src/main/java/amrobot/apigateway/bungie/data/AggregateStatsData.java
+++ src/main/java/amrobot/apigateway/bungie/data/AggregateStatsData.java
@@ -1,4 +1,4 @@
-package amrobot.apigateway.data.bungie;
+package amrobot.apigateway.bungie.data;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
