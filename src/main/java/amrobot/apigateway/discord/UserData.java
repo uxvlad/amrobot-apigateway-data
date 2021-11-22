fd586ef27e641a9dc66854b4a17c21c09499d40e
Vladislavs Zaharovs <vladislav.zaharov@gmail.com>
2021-11-22 02:01:31 +0200

    Update user data

--- src/main/java/amrobot/apigateway/discord/UserData.java
+++ src/main/java/amrobot/apigateway/discord/UserData.java
@@ -1,17 +1,26 @@
 package amrobot.apigateway.discord;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import javax.annotation.Nullable;
 import org.immutables.value.Value.Immutable;
 
 @Immutable
+@JsonSerialize(as = ImmutableUserData.class)
+@JsonDeserialize(as = ImmutableUserData.class)
 public interface UserData {
 
   String uid();
+
+  @Nullable
+  String username();
 
   @JsonProperty("destiny2_id")
   Long destiny2id();
 
   @JsonProperty("destiny2_uid")
+  @Nullable
   String destiny2uid();
 
   static ImmutableUserData.Builder builder() {
